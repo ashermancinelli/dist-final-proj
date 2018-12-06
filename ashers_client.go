dd4f66620219e2ed027ed78404287b34e2b1b301
Asher Mancinelli <ashermancinelli@gmail.com>
2018-12-06 14:00:11 -0800

    added multi host

--- ashers_client.go
+++ ashers_client.go
@@ -7,20 +7,24 @@ import (
 	"log"
 	"net"
 	"os"
+	"os/exec"
 	"strconv"
 	"strings"
 )
 
+const (
+	usageString string = "\n\nPossible commands:\nhelp\t\tDisplay this message.\nlist\t\tList all active players.\nattack\t\tAttack some player.\nname\t\tSet your name. Can only be called once.\nscore\t\tOutput current score of game."
+)
+
 var (
-	allPlayers    []string
 	alivePlayers  []string
 	deadPlayers   []string
+	allPlayers    []string
 	myName        string = "placeholder"
 	nameSet       bool   = false
 	gameActive    bool   = false
 	myHealth      int    = 100
 	mykiller      string = "stillAlive"
-	killed        int    = 0
 	spectatorMode bool   = false
 )
 
@@ -30,8 +34,9 @@ func handleGameString(str string) []byte { //handles relevant string data from m
 
 	finalValue := "\n"
 	switch {
-	case commands[0] == "error", commands[0] == "name":
-		break
+	case commands[0] == "name":
+		allPlayers = append(allPlayers, commands[1])
+		log.Println("New player added.")
 	case commands[0] == "meta": //a message type for anything else
 		switch {
 		case commands[1] == "all players":
@@ -59,7 +64,6 @@ func handleGameString(str string) []byte { //handles relevant string data from m
 			case "end":
 				gameActive = false
 				finalValue = "Game over!\n"
-
 			}
 
 		default:
@@ -75,7 +79,7 @@ func handleGameString(str string) []byte { //handles relevant string data from m
 		gameActive = false
 	case commands[0] == "death": //reports another players death
 		if len(commands) < 3 { //error check
-			return []byte("error;bad args;kill\n")
+			return []byte("error;bad args;death\n")
 		}
 		finalValue = fmt.Sprint("Player ", commands[2], " has been killed by ", commands[1], "\n") //output who died
 	case commands[0] == "attack": //a player was attacked
@@ -97,7 +101,7 @@ func handleGameString(str string) []byte { //handles relevant string data from m
 		}
 
 	default:
-		finalValue = "error;command_not_implemented\n"
+		finalValue = "error;command not implemented\n"
 		log.Print("Bad game data: ", str, "\n")
 	}
 	return []byte(finalValue)
@@ -107,19 +111,67 @@ func handleInputString(str string) []byte {
 	str = strings.TrimSpace(str)
 	commands := strings.Split(str, " ")
 
-	finalValue := "\n"
+	finalValue := ""
 	switch commands[0] {
+	case "help":
+		log.Print(usageString)
+	case "raw":
+		finalValue = commands[1] + "\n"
+	case "clear":
+		cmd := exec.Command("clear")
+		cmd.Run()
 	case "attack":
-		finalValue = fmt.Sprint("attack;", commands[1], ";", myName, ";100\n")
+		if !gameActive {
+			log.Print("Game has not started yet!\n")
+		} else if myHealth == 0 {
+			log.Print("You are dead and cannot attack anymore!\n")
+		} else if len(commands) < 3 {
+			log.Print("You have not given enough arguments!\n")
+			log.Print("Format: attack [PLAYER NAME] [DAMAGE]\n")
+		} else {
+			finalValue = fmt.Sprint("attack;", commands[1], ";", myName, ";", commands[2], "\n")
+		}
 	case "name":
-		if !nameSet {
+		if len(commands) < 2 {
+			log.Println("My name: ", myName)
+			break
+		}
+
+		if nameSet {
+			log.Print("You can only set your name once!\n")
+		} else if len(commands[1]) > 5 {
+			log.Print("Your name must be 5 characters or less.\n")
+		} else {
+			found := false
+			for _, v := range allPlayers {
+				if v == commands[1] {
+					log.Println("Another player has already taken that name!")
+					found = true
+				}
+			}
+			if found {
+				break
+			}
 			nameSet = true
 			myName = commands[1]
 			finalValue = fmt.Sprint("name;", commands[1], "\n")
-		} else {
-			log.Print("You can only set your name once!\n")
+			log.Println("Name has successfully been set.")
 		}
+	case "list":
+		if len(allPlayers) == 0 {
+			log.Println("No other players have registered on this server yet.")
+		} else {
+			log.Print("My name: ", myName, "\n")
+			log.Print("All names:\n")
+			for i, v := range allPlayers {
+				log.Print("Name ", i, ": ", v)
+			}
+		}
+	case "score":
+		log.Println("Score not implemented yet...")
 	default:
+		log.Print("Error: Bad input.\n")
+		log.Println(usageString)
 		finalValue = fmt.Sprint("error;bad_input_string;", str, "\n")
 	}
 
@@ -149,15 +201,17 @@ func streamCpy(src io.Reader, dst io.Writer, isOutgoing bool) <-chan int {
 				break
 			}
 
+			var data []byte
 			if isOutgoing {
 				str := string(buf[0:nBytes])
-				data := handleInputString(str)
-				_, err = dst.Write(data)
+				data = handleInputString(str)
 			} else {
 				str := string(buf[0:nBytes])
-				data := handleGameString(str)
-				_, err = dst.Write(data)
+				data = handleGameString(str)
 			}
+
+			_, err = dst.Write(data)
+
 			if err != nil {
 				log.Fatalf("Write error: %s\n", err)
 			}
@@ -188,6 +242,8 @@ func StartClient(host string, port string) {
 	}
 
 	log.Println("Connected to ", host+port)
+	log.Println("Welcome to GoWar!!", usageString)
+	allPlayers = append(allPlayers, "admin")
 	HandleCons(con)
 }
 
---
+++ multi_host.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+var allClients map[*Client]int
+
+type Client struct {
+	// incoming chan string
+	outgoing   chan string
+	reader     *bufio.Reader
+	writer     *bufio.Writer
+	conn       net.Conn
+	connection *Client
+}
+
+func (client *Client) Read() {
+	for {
+		line, err := client.reader.ReadString('\n')
+		if err == nil {
+			if client.connection != nil {
+				client.connection.outgoing <- line
+			}
+			fmt.Println(line)
+		} else {
+			break
+		}
+
+	}
+
+	client.conn.Close()
+	delete(allClients, client)
+	if client.connection != nil {
+		client.connection.connection = nil
+	}
+	client = nil
+}
+
+func (client *Client) Write() {
+	for data := range client.outgoing {
+		client.writer.WriteString(data)
+		client.writer.Flush()
+	}
+}
+
+func (client *Client) Listen() {
+	go client.Read()
+	go client.Write()
+}
+
+func NewClient(connection net.Conn) *Client {
+	writer := bufio.NewWriter(connection)
+	reader := bufio.NewReader(connection)
+
+	client := &Client{
+		// incoming: make(chan string),
+		outgoing: make(chan string),
+		conn:     connection,
+		reader:   reader,
+		writer:   writer,
+	}
+	client.Listen()
+
+	return client
+}
+
+func main() {
+	allClients = make(map[*Client]int)
+	listener, _ := net.Listen("tcp", ":2007")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		client := NewClient(conn)
+		for clientList, _ := range allClients {
+			if clientList.connection == nil {
+				client.connection = clientList
+				clientList.connection = client
+				fmt.Println("Connected")
+			}
+		}
+		allClients[client] = 1
+		fmt.Println(len(allClients))
+	}
+}
